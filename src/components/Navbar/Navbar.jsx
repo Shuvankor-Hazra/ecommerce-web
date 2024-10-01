8280264cf7a0d17f9bae0f009bf2b68d92c38163
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-10-02 01:55:18 +0600

    e-comerce-web navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                     {/* Search bar and order button */}
                     <div className="flex items-center justify-between gap-4">
                         <div className="group relative hidden sm:block">
-                            <input type="text" placeholder="Search" className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-3 py-1 focus:outline-none focus:border-1 focus:border-primary" />
+                            <input type="text" placeholder="Search" className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-3 py-1 focus:outline-none focus:border-1 focus:border-primary focus:w-[300px]" />
                             <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
                         </div>
 
