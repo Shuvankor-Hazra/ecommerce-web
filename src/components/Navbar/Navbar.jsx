f46d08d30d9ba8410bca13dbee17b24123abdad0
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-10-03 00:52:49 +0600

    Navbar bottom added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,53 @@
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import { IoMdSearch } from "react-icons/io";
 import logo from "../../assets/logo.png";
 import DarkMode from "./DarkMode";
+
+const Menu = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/#"
+    },
+    {
+        id: 2,
+        name: "Top Rated",
+        link: "/#"
+    },
+    {
+        id: 3,
+        name: "Kids Wear",
+        link: "/#"
+    },
+    {
+        id: 4,
+        name: "Mens Wear",
+        link: "/#"
+    },
+    {
+        id: 5,
+        name: "Electronics",
+        link: "/#"
+    },
+]
+
+const DropdownItems = [
+    {
+        id: 1,
+        name: "Trending Products",
+        link: "/#",
+    },
+    {
+        id: 2,
+        name: "Best Selling",
+        link: "/#",
+    },
+    {
+        id: 3,
+        name: "Top Rated",
+        link: "/#",
+    },
+]
 
 const Navbar = () => {
     return (
@@ -15,14 +61,15 @@ const Navbar = () => {
                             Shopping
                         </a>
                     </div>
-
                     {/* Search bar and order button */}
                     <div className="flex items-center justify-between gap-4">
                         <div className="group relative hidden sm:block">
-                            <input type="text" placeholder="Search" className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-3 py-1 focus:outline-none focus:border-1 focus:border-primary focus:w-[300px]" />
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-3 py-1 focus:outline-none focus:border-1 focus:border-primary focus:w-[300px] dark:border-gray-800 dark:bg-gray-500" />
                             <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
                         </div>
-
                         {/* Order button */}
                         <button
                             onClick={() => alert('Ordering not available yet')}
@@ -31,17 +78,48 @@ const Navbar = () => {
                             <span className="group-hover:block hidden transition-all duration-200">Order</span>
                             <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer my-1" />
                         </button>
-
                         {/* Dark mode switch */}
                         <div>
                             <DarkMode />
                         </div>
                     </div>
-
                 </div>
             </div>
             {/* Lower navbar */}
-            <div></div>
+            <div className="flex justify-center">
+                <ul className="sm:flex items-center gap-4 hidden">
+                    {Menu.map(data => (
+                        <li key={data.id}>
+                            <a className="inline-block px-4 hover:text-primary duration-200"
+                                href={data.link}>
+                                {data.name}
+                            </a>
+                        </li>
+                    ))}
+                    {/* Simple dropdown and links */}
+                    <li className="group relative cursor-pointer">
+                        <a className="flex items-center gap-[2px] py-2"
+                            href='#'>
+                            Trending Products
+                            <span>
+                                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+                            </span>
+                        </a>
+                        <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+                            <ul>
+                                {DropdownItems.map(data => (
+                                    <li key={data.id}>
+                                        <a className="inline-block rounded-md p-2 hover:bg-primary/20"
+                                            href={data.link}>
+                                            {data.name}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
