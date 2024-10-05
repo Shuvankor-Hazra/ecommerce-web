2f539a37ae2dbc310d45f7db94a5123cabededca
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-10-05 13:18:46 +0600

    fixed some changes

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -86,7 +86,7 @@ const Products = () => {
                     {/* View All Button */}
                     <div className="flex justify-center">
                         <button className="text-center mt-10 cursor-pointer bg-primary text-white py-1 px-5 rounded-md hover:scale-105 duration-200">
-                            View All Button
+                            View All
                         </button>
                     </div>
                 </div>
